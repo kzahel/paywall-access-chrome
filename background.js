58a0cf1488cc5847b81491a15a3251ebcc719f92
Kyle Graehl <kgraehl@gmail.com>
2015-10-18 15:24:33 -0700

    cleanup

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-
-
 chrome.contextMenus.create({id:"paywall",
                             title:chrome.i18n.getMessage('extName'),
                             contexts:["all"]})
@@ -16,35 +14,27 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
                             
 
 
-  chrome.browserAction.onClicked.addListener(function(tab) {
-      // access current tab data
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // access current tab data
+    var content_script_fn = (function(config) {
+        console.log("activeTab executeScript code");
+        chrome.runtime.sendMessage("adpfpconnigelofkfenblmdhbnlaafkg", {
+            title: document.title
+        })
+    })
 
-      var content_script_fn = (function(config) {
-          console.log("activeTab executeScript code");
-          chrome.runtime.sendMessage("adpfpconnigelofkfenblmdhbnlaafkg", {
-              title: document.title
-          })
-      })
-
-      var config = {testint:123,teststring:'bananas'}
-      
-      var code = '(' + content_script_fn.toString() + ')' + '('+JSON.stringify(config)+')';
-
-      //var code = 'console.log("hello;")';
-      
-      console.log('injecting code:',code)
-
-      chrome.tabs.executeScript(null, {code: code});
-      
-    //chrome.windows.create({"url": "https://googletab.url", "incognito": true});
-
-  });
+    var config = {testint:123,teststring:'bananas'}
+    var code = '(' + content_script_fn.toString() + ')' + '('+JSON.stringify(config)+')';
+    console.log('injecting code:',code)
+    chrome.tabs.executeScript(null, {code: code});
+});
 
 chrome.runtime.onMessage.addListener(function(msg){
     console.log('runtime message',msg)
     if (msg.title) {
         //https://www.google.com/search?sclient=psy-ab&site=&source=hp&q=Trump%2C+Bush+Intensify+Fight+Over+9%2F11%2C+Foreign+Policy&btnI=I%27m+Feeling+Lucky
         if (false) {
+            // does not work, have to actually visit the google search results page. hm
             var url = "https://www.google.com/search?" +
                 'sclient=psy-ab&site=&source=hp' +
                 "&q=" + encodeURIComponent(msg.title) + "&btnI=I%27m+Feeling+Lucky"
